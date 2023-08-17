c050233b9f8fc3dd935e85f5791cadca50f7b341
Jheimys Bassani <jheimysbb@gmail.com>
2023-08-17 10:37:56 +0100

    :construction: Criando os componetes de Banner, Card, Rodape e Titulo

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,20 @@
+import Banner from "../../componentes/Banner"
+import Card from "../../componentes/Card"
+import Rodape from "../../componentes/Rodape"
+import Titulo from "../../componentes/Titulo"
 import Cabecalho from "../../componentes/cabecalho"
 
 function Inicio() {
     return(
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Banner imagem='home'/>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <Card id = '1' titulo='bjabja' capa ='https://conteudo.imguol.com.br/c/entretenimento/36/2022/05/22/gata-tricolor-gato-gatos-1653265224214_v2_1x1.jpg' />
+            <Rodape />
+        </>
     )
 }
 
