6368457ee684393148be69577a42d1de043e3902
Jheimys Bassani <jheimysbb@gmail.com>
2023-09-05 16:13:24 +0100

    :construction: Adicionando o componente de Rodape

--- src/pages/PaginaBase/index.js
+++ src/pages/PaginaBase/index.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Container from "../../componentes/Container";
 import Cabecalho from "../../componentes/cabecalho";
 import FavoritosProvaider from "../../context/Favoritos";
+import Rodape from "../../componentes/Rodape";
 
 
 function PaginaBase() {
@@ -13,6 +14,7 @@ function PaginaBase() {
                     <Outlet />
                 </Container>
             </FavoritosProvaider>
+            <Rodape />
         </main>
     )
 }
